c60c5611ce1ae8422c526de38fe3507d365b93db
jiun <vonreal20@gmail.com>
2022-08-08 09:29:31 +0900

    Initial Commit

---
+++ sesac-Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesac-Week6
+//
+//  Created by 나지운 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
