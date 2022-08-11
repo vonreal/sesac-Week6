72114a441a795691bc9b26dfb79e99e98e1ca2dd
jiun <vonreal20@gmail.com>
2022-08-11 11:10:46 +0900

    🌱 [0811 - 수업]: 지도에 핀 추가

--- sesac-Week6/View Controllers/MapViewController.swift
+++ sesac-Week6/View Controllers/MapViewController.swift
@@ -23,10 +23,37 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 지도 중심 설정: 애플맵 활용해 좌표 복사
-        let center = CLLocationCoordinate2D(latitude: 34.811635, longitude: 126.425222)
-        
-        // 지도 중심 기반으로 보여질 범위 설정
-        MKCoordinateRegion(center: center, latitudinalMeters: 100, longitudinalMeters: 100)
+        setRegionAndAnnotation()
     }
+}
+
+func setRegionAndAnnotation() {
+    // 지도 중심 설정: 애플맵 활용해 좌표 복사
+    let center = CLLocationCoordinate2D(latitude: 34.811635, longitude: 126.425222)
+    
+    // 지도 중심 기반으로 보여질 범위 설정
+    let region = MKCoordinateRegion(center: center, latitudinalMeters: 100, longitudinalMeters: 100)
+    
+    mapView.setRegion(region, animated: true)
+    
+    
+    // 지도에 핀 추가
+    let annotation =  MKPointAnnotation()
+    
+    annotation.coordinate = center
+    annotation.title = "가야 할 곳"
+    
+    mapView.addAnnotation(annotation)
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    // 지도에 커스텀 핀 추가 -> kakao taxi, naver map 버스 등의 표시
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        <#code#>
+//    }
+    
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        <#code#>
+//    }
 }
