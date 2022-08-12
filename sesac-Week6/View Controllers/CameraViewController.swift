078ad16342c4368ff1168e63332d7164e40f946d
jiun <vonreal20@gmail.com>
2022-08-12 11:31:01 +0900

    🌱 [0812 - 수업]: UIImagePickerController를 이용해서 카메라 및 포토 라이브러리 띄우기

--- sesac-Week6/View Controllers/CameraViewController.swift
+++ sesac-Week6/View Controllers/CameraViewController.swift
@@ -7,15 +7,23 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+import Toast
 import YPImagePicker
 
 class CameraViewController: UIViewController {
 
     @IBOutlet weak var photoImageView: UIImageView!
     
+    // UIImagePickerController - 1
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // UIImagePickerController - 2
+        picker.delegate = self
     }
     
     // OpenSource
@@ -42,9 +50,61 @@ class CameraViewController: UIViewController {
     
     // UIImagePickerController
     @IBAction func cameraButtonClicked(_ sender: UIButton) {
+        // 카메라 사용 가능 여부 판단
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            self.view.makeToast("카메라가 사용이 불가능해유!")
+            return
+        }
+        
+        picker.sourceType = .camera
+        picker.allowsEditing = true // default: false, 정방향으로 사진을 크롭하는 사각형이 나온다.
+        
+        present(picker, animated: true)
     }
     
     // UIImagePickerController
     @IBAction func photoLibraryButtonClicked(_ sender: UIButton) {
+        // 포토 라이브러리 사용 가능 여부 판단
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            self.view.makeToast("포토 라이브러리가 사용이 불가능해유!")
+            return
+        }
+        
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true)
+    }
+    
+    @IBAction func saveToPhotoLibrary(_ sender: UIButton) {
+        if let image = photoImageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            self.view.makeToast("이미지가 저장되었슈!")
+        } else {
+            self.view.makeToast("photoImageView에 이미지가 읍슈!")
+        }
+    }
+    
+}
+
+// UIImagePickerController - 3
+// 네비게이션 컨트롤러를 상속받고 있음.
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // UIImagePickerController - 4 : 갤러리에서 사진을 선택하거나 카메라 촬영 후 사진을 USE PHOTO를 클릭했을때
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // 원본, 편집, 메타 데이터 등 - infoKey
+        if let image = info[.originalImage] as? UIImage {
+            self.photoImageView.image = image
+            dismiss(animated: true)
+        } else {
+            view.makeToast("저장할 이미지가 없슈!")
+        }
+        print(#function)
+    }
+    
+    // UIImagePickerController - 5 : 좌측 상단 취소버튼 눌렀을 때
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print(#function)
     }
 }
