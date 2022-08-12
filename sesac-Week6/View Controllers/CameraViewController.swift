3f8e47060a88a998ddb481ddcee8c0932adfc482
jiun <vonreal20@gmail.com>
2022-08-12 10:34:42 +0900

    🌱 [0812 - 수업]: YPImagePicker 데이터 이용하여 imageView에 띄우기

--- sesac-Week6/View Controllers/CameraViewController.swift
+++ sesac-Week6/View Controllers/CameraViewController.swift
@@ -11,6 +11,8 @@ import YPImagePicker
 
 class CameraViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,8 +22,9 @@ class CameraViewController: UIViewController {
     // 권한은 다 허용 해주세요!
     // 실제 디바이스 빌드 해보세용. 시뮬레이터(카메라X)
     // 실제로 카메라를 쓸 때 권한 요청
+    // 권한 문구 등도 내부적으로 구현
     @IBAction func YPImagePickerButtonClicked(_ sender: UIButton) {
-        let picker = YPImagePicker() // 권한 문구 등도 내부적으로 구현
+        let picker = YPImagePicker()
         picker.didFinishPicking { [unowned picker] items, _ in
             if let photo = items.singlePhoto {
                 print(photo.fromCamera) // Image source (camera or library)
@@ -29,6 +32,8 @@ class CameraViewController: UIViewController {
                 print(photo.originalImage) // original image selected by the user, unfiltered
                 print(photo.modifiedImage) // Transformed image, can be nil
                 print(photo.exifMeta) // Print exif meta data of original image.
+                
+                self.photoImageView.image = photo.image
             }
             picker.dismiss(animated: true, completion: nil)
         }
